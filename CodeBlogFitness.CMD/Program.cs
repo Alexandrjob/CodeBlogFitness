75b43be4791bed531de844e210052745046b415c
Alexandr <60702539+Alexandrjob@users.noreply.github.com>
2020-04-01 14:06:40 +1000

    Добавил локализацию.

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -1,28 +1,34 @@
 ﻿using CodeBlogFitness.BL.Controller;
 using CodeBlogFitness.BL.Model;
 using System;
+using System.Globalization;
+using System.Resources;
 
 namespace CodeBlogFitness.CMD
 {
     class Program
     {
+        static CultureInfo culture = new CultureInfo("ru"); //.CreateSpecifiCulture("ru");
+        
+        static ResourceManager resurceManager = new ResourceManager("CodeBlogFitness.CMD.Languages.Messages", typeof(Program).Assembly);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Запуск приложения *****");
-            Console.WriteLine("Вас приветствую Я");
+            Console.WriteLine(resurceManager.GetString("Status", culture));
+            Console.WriteLine(resurceManager.GetString("Hello", culture));
 
-            Console.WriteLine("Введите имя пользователя: ");
+            Console.WriteLine(resurceManager.GetString("EnterName", culture));
             var name = Console.ReadLine();
 
             var userController = new UserController(name);
 
             if (userController.IsNewUser)
             {
-                Console.WriteLine("Введите пол: ");
+                Console.WriteLine(resurceManager.GetString("EnterGender", culture));
                 var gender = Console.ReadLine();
 
-                double weight = ParseDouble("вес");
-                double height = ParseDouble("рост");
+                double weight = ParseDouble(resurceManager.GetString("EnterWeight", culture));
+                double height = ParseDouble(resurceManager.GetString("EnterHeight", culture));
 
                 DateTime birthDate = ParseDateTime();
 
@@ -33,8 +39,8 @@ namespace CodeBlogFitness.CMD
             Console.WriteLine(userController.CurrentUser);
             var eatingController = new EatingController(userController.CurrentUser);
 
-            Console.WriteLine("Что вы хотите сделать?");
-            Console.WriteLine("E - ввести прием пищи");
+            Console.WriteLine(resurceManager.GetString("WhatToDo", culture));
+            Console.WriteLine(resurceManager.GetString("EnterMeal", culture));
             var key = Console.ReadKey();
             Console.WriteLine();
 
@@ -54,15 +60,15 @@ namespace CodeBlogFitness.CMD
 
         private static (Food Food, double Weight) EnterEating()
         {
-            Console.Write("Введите имя продукта: ");
+            Console.Write("{0}: ", resurceManager.GetString("EnterProductName", culture));
             var food = Console.ReadLine();
 
-            var calories = ParseDouble("калорийность");
-            var prots = ParseDouble("белки");
-            var fats = ParseDouble("жиры");
-            var carbs = ParseDouble("углеводы");
+            var calories = ParseDouble(resurceManager.GetString("Calorie", culture));
+            var prots = ParseDouble(resurceManager.GetString("Protein", culture));
+            var fats = ParseDouble(resurceManager.GetString("Fats", culture));
+            var carbs = ParseDouble(resurceManager.GetString("Carbohydrates", culture));
 
-            var weight = ParseDouble("вес порции");
+            var weight = ParseDouble(resurceManager.GetString("EnterServingWeight", culture));
             var product = new Food(food, calories, prots, fats, carbs);
 
 
@@ -92,7 +98,8 @@ namespace CodeBlogFitness.CMD
         {
             while (true)
             {
-                Console.WriteLine($"Введите {name}: ");
+                //Console.WriteLine($"Введите {name}: ");
+                Console.WriteLine("{0} {1}: ", resurceManager.GetString("Enter", culture), name);
                 if (double.TryParse(Console.ReadLine(), out double value))
                 {
                     return value;
